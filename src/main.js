7f2b2821422cabacce73d23079e721b7f955e851
Andrey Reutov <reutov.andrey@gmail.com>
2019-11-12 18:44:18 +0300

    added users page

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
+//import 'bootstrap/dist/css/bootstrap.css'
+import VTooltip from 'v-tooltip'
+import Copy from 'v-copy'
 
 Vue.config.productionTip = false
+
+Vue.use(VTooltip)
+Vue.use(Copy)
 
 new Vue({
   router,

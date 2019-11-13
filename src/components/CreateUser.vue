bd9ede9f37f1bf24dae94b73732a64b1205b590d
Reutov Andrey <reutov.andrey@gmail.com>
2019-11-13 21:45:11 +0300

    Add submit functionality on add user page

--- src/components/CreateUser.vue
+++ src/components/CreateUser.vue
@@ -1,22 +1,79 @@
 <template>
-  <div class="form">
+ 
+    <form @submit="sendForm">
+    <p v-if="errors.length">
+      <b>Пожалуйста исправьте указанные ошибки:</b>
+      <ul>
+        <li v-for="error in errors" :key="error">{{ error }}</li>
+      </ul>
+    </p>
     <p>Имя пользователя</p>
-    <input v-model="firstName" placeholder="Введите имя пользователя" />
+    <input v-model="user.firstName" placeholder="Введите имя пользователя" />
     <p>Фамилия пользователя</p>
-    <input v-model="lastName" placeholder="Введите фамилию пользователя" />
-    <input id="checkbox" v-model="isActive" type="checkbox" />
+    <input v-model="user.lastName" placeholder="Введите фамилию пользователя" />
+    <input id="checkbox" v-model="user.isActive" type="checkbox" />
     <label for="checkbox">Активен</label>
     <p>Начальный баланс</p>
-    <input v-model="balance" placeholder="Введите баланс пользователя" />
+    <input v-model="user.balance" placeholder="Введите баланс пользователя" />
     <p>Email</p>
-    <input v-model="email" placeholder="Введите email пользователя" />
+    <input v-model="user.email" placeholder="Введите email пользователя" />
     <p>Phone</p>
-    <input v-model="phone" placeholder="Введите телефон пользователя" />
-  </div>
+    <input v-model="user.phone" placeholder="Введите телефон пользователя" />
+    
+    <p> <input type="submit" value="Создать"> </p>
+  </form>
+ 
 </template>
 
 <script>
-export default {}
+import axiosInstance from '@/utils/http.js'
+export default {
+  data() {
+    return {
+      errors: [],
+      user: {
+        firstName: null,
+        lastName: null,
+        isActive: false,
+        balance: 0,
+        email: null,
+        phone: null
+      }
+    }
+  },
+   methods: {
+    sendForm: async function (e) {
+      e.preventDefault();
+      
+      const {firstName, lastName, email, phone} = this.user
+
+      if (firstName && lastName && email && phone) {
+        // TODO: обернуть в try - catch, показывать сообщение об успешном создании
+        await axiosInstance.post('/users', JSON.stringify(this.user), {
+          headers: { 'content-type': 'application/json' }
+        } )
+        this.$router.push('/')
+        return true;
+      }
+
+      this.errors = [];
+
+      if (!this.firstName) {
+        this.errors.push('Требуется указать имя.');
+      }
+      if (!this.lastName) {
+        this.errors.push('Требуется указать фамилию.');
+      }
+      if (!this.email) {
+        this.errors.push('Требуется указать email.');
+      }
+        if (!this.phone) {
+        this.errors.push('Требуется указать телефон.');
+      }
+      
+    }
+  }
+}
 </script>
 
 <style></style>
